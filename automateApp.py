99a17a2f9c64375b96e3418efe7d03ec0ce3ad32
maja <marian89pl@gmail.com>
2019-10-28 21:32:40 +0100

    Test

---
+++ automateApp.py
@@ -0,0 +1,27 @@
+import subprocess, sys
+import pyautogui
+import pyperclip
+
+# powershellVirtualAndroidScript = "F:\\memu\\runemucMultipleTimes.ps1"
+# subprocess.Popen(["powershell", powershellVirtualAndroidScript])
+#will run pythong from ps1 script
+
+screenWidth, screenHeight    = pyautogui.size()
+currentMouseX, currentMouseY = pyautogui.position()
+# pyautogui.moveTo(450, 450, duration=1.0)
+num = 0
+while num < 100 :
+    print(pyautogui.position)
+    tmp_var = input()
+    num = num + 1
+
+# pyautogui.click()
+# pyautogui.moveRel(None, 10)  # move mouse 10 pixels down
+# pyautogui.doubleClick()
+# pyautogui.moveTo(500, 500, duration=2, tween=pyautogui.easeInOutQuad)  # use tweening/easing function to move mouse over 2 seconds.
+# pyautogui.typewrite('Hello world!', interval=0.25)  # type with quarter-second pause in between each key
+# pyautogui.press('esc')
+# pyautogui.keyDown('shift')
+# pyautogui.press(['left', 'left', 'left', 'left', 'left', 'left'])
+# pyautogui.keyUp('shift')
+# pyautogui.hotkey('ctrl', 'c')
