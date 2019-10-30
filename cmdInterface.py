07d6b253f34e4db651b82bde6342dcf66f3a3589
maja <marian89pl@gmail.com>
2019-10-30 19:47:34 +0100

    added class to cmdInterface

--- cmdInterface.py
+++ cmdInterface.py
@@ -1,3 +1,4 @@
 import argparse
-small_interface = argparse.ArgumentParser(description='Automate process of aquarying quidian stones')
-small_interface.add_argument('--r', action='store_false', default=True, help="If you want to record new position of widnows add --r to command line")
+class CmdInterface:
+    small_interface = argparse.ArgumentParser(description='Automate process of aquarying quidian stones')
+    small_interface.add_argument('--r', action='store_true', default=False, help="If you want to record new position of widnows add --r to command line")
