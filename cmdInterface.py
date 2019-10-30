951576264eb2220b0b06d43f4aaea26dfcbce046
maja <marian89pl@gmail.com>
2019-10-30 17:43:10 +0100

    Created interface

---
+++ cmdInterface.py
@@ -0,0 +1,3 @@
+import argparse
+small_interface = argparse.ArgumentParser(description='Automate process of aquarying quidian stones')
+small_interface.add_argument('--r', action='store_false', default=True, help="If you want to record new position of widnows add --r to command line")
