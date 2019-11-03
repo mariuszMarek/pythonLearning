936cc0ead906065d6e26c8bdbd82748708314dab
maja <mariusz.marek.olszewski@gmail.com>
2019-11-03 12:43:07 +0100

    update and cleaned the file of ussless code

--- mouse_keybord_events.py
+++ mouse_keybord_events.py
@@ -3,37 +3,19 @@ from pynput import keyboard
 
 
 def mouse_events(position_list = []):
-    # def on_move(x, y):
-    #     print('Pointer moved to {0}'.format( (x, y) ) )
     def on_click(x, y, button, pressed):
         # print('{0} at {1}'.format( 'Pressed' if pressed else 'Released', (x, y) ) )
         if pressed : position_list.append(tuple((x, y)))
-        # if not pressed: ?
-            # Stop listener
-            # return False
-     # def on_scroll(x, y, dx, dy):
-        # print('Scrolled {0} at {1}'.format( 'down' if dy < 0 else 'up', (x, y) ) )
-
-    # in a non-blocking fashion litener:
-    listener1 = mouse.Listener( on_click=on_click )
-    listener1.start()
-    def on_press(key):
-        try:
-            print('alphanumeric key {0} pressed'.format(
-                key.char))
-        except AttributeError:
-            print('special key {0} pressed'.format(
-                key))
-
-
+    
     def on_release(key):
-        print('{0} released'.format(
-            key))
         if key == keyboard.Key.esc:
-            # Stop listener
+            print('{0} released, stipping recording'.format( key ) )
+            listener1.stop()
+            listener2.stop()
             return False
-    # ...or, in a non-blocking fashion:
-    listener2 = keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release)
+            
+    # in a non-blocking fashion:
+    listener1 = mouse.Listener   ( on_click=on_click )    
+    listener2 = keyboard.Listener( on_release=on_release )
+    listener1.start()
     listener2.start()
