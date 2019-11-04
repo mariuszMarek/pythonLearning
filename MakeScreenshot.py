c30b1bfbac19fbdf55c41f36e3f4dd9a5ad30ae7
maja <mariusz.marek.olszewski@gmail.com>
2019-11-04 12:39:32 +0100

    added new class and finished ManageSettings

---
+++ MakeScreenshot.py
@@ -0,0 +1,5 @@
+    # def make_screensots(self):    
+    #     # tutaj zrobic screenshoty do tego ? to juz moze oddzielna klasa/funkcja
+    #     pass
+    #     for position_
+    #     # PIL.ImageGrab.grab
--- ManagaSettings.py
+++ ManagaSettings.py
@@ -5,7 +5,7 @@ from mouse_keybord_events import MouseEvents
 
 
 class ManageSettings:    
-    def __init__(self, record_or_not):
+    def __init__(self, record_or_not = False):
         self.to_record     = record_or_not
         self.autogui       = pyautogui
         self.checkinput    = pynput
@@ -18,27 +18,27 @@ class ManageSettings:
             else:
                 self.load_settings()                
     def save_settings(self):
-        with open(self.ini_path, 'w') as file_settings:
+        with open(self.ini_path, 'w') as file_writter:
             for posXY in self.position_list:
-                file_settings.write("{};{}".fo  rmat( posXY[0], posXY[1] ))        
-        pass
+                file_writter.write("{};{}\n".format(posXY[0], posXY[1]))
     def load_settings(self):
-        #tutaj trzeba odczytac utawienia
-        pass
+        with open(self.ini_path, 'r') as file_reader:
+            for line_XY in file_reader:
+                posX, posY = line_XY.split(";")
+                self.position_list.append( tuple ( (posX,posY) ) )
     @property
     def position_list(self):
-        return self.position_list
-    def make_screensots(self):    
-        # tutaj zrobic screenshoty do tego ? to juz moze oddzielna klasa/funkcja
-        pass
-        # for position_
-        # PIL.ImageGrab.grab
+        return self.__position_list
+    @position_list.setter
+    def position_list(self, position_list):
+        self.__position_list = position_list
+
     
-#simple unit test
-# testKlas = ManageSettings(True)
+# simple unit test
+# testKlas = ManageSettings()
 # testKlas.save_or_load()
 # for posXY in testKlas.position_list:
-#     print("pozycja x->{} y->{}".format(posXY[0],posXY[1]))
+#     print("pozycja x->{} y->{}".format(posXY[0],posXY[1]), end="")
     
 
 
