43243799a180c1bf97c9aa402feac8e6a9f35129
maja <mariusz.marek.olszewski@gmail.com>
2019-11-06 23:33:14 +0100

    finished work on passcode finder. Small updates in readinOfEmail

--- passcodeFinder.py
+++ passcodeFinder.py
@@ -1,21 +1,23 @@
 from readinOfEmail import ReadEmails
-import time
 
 class ParsEmail(ReadEmails):
-    def __init__(self):        
+    def __init__(self):
         ReadEmails.__init__(self)
-        self.ParenClas = ReadEmails()
+        self.ParenClas     = ReadEmails()
         self.list_of_email = self.get_emails()
-    def find_passcode(self):
+        self.email_to      = ""
+        self.passcode      = ""
+        self.email_from    = ""
+
+    def find_passcode(self, email_from, email_to, subjec_to_find):
         for message in self.list_of_email:
-            msg = self.ParenClas.service.users().messages().get(userId='me', id=message['id'],format='full').execute()
+            msg                = self.ParenClas.service.users().messages().get( userId='me', id=message['id'], format='full').execute() #gets the individual message from email            
+            correct_email_from = False
+            correct_email_to   = False
+            correct_subject    = False
             for email_subject in msg['payload']['headers']:
-                # print(email_subject)
-                if email_subject['name'] == 'From' and email_subject['value'] == 'Webnovel <noreply@webnovel.com>':
-                    print(email_subject['value'])
-                    print(msg['snippet'])
-                    # print(email_subject)
-                    # print(msg['payload']['parts']['0']['body']['data'])
-                    time.sleep(10)
-test = ParsEmail()
-test.find_passcode()
+                if email_subject['name'] == 'From'    and email_subject['value'] == email_from:     correct_email_from = True
+                if email_subject['name'] == 'To'      and email_subject['value'] == email_to:       correct_email_to   = True
+                if email_subject['name'] == 'Subject' and email_subject['value'] == subjec_to_find: correct_subject    = True
+            if correct_email_from and correct_email_to and correct_subject:
+                return msg['snippet'].split('|')[0].split(',')[0].split(':')[1].split(" ")[1] # it's the code for confirmation
