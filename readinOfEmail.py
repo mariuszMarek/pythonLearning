5179403b7026e46010c6d5de922705613e2ca693
maja <mariusz.marek.olszewski@gmail.com>
2019-11-06 20:59:24 +0100

    added json token and reader of emails

---
+++ readinOfEmail.py
@@ -0,0 +1,19 @@
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+class ReadEmails:
+    def __init__(self, scope='https://www.googleapis.com/auth/gmail.readonly', jsonLocation='F:\\python\\automateWork\\credentials.json'):
+        self.SCOPES = scope
+        self.store  = file.Storage('token.json')
+        self.creds  = self.store.get()
+
+        if not self.creds or self.creds.invalid:
+            flow       = client.flow_from_clientsecrets(jsonLocation, self.SCOPES)
+            self.creds = tools.run_flow(flow, self.store)
+        self.service   = build('gmail', 'v1', http=self.creds.authorize(Http() ) )
+    def get_emails(self):
+        # Call the Gmail API to fetch INBOX
+        results = self.service.users().messages().list(userId='me', labelIds=['INBOX']).execute()
+        # results = self.service.users().messages().get(userId='me', labelIds=['INBOX'], format=None, metadataHeaders=None).execute()
+        return results.get('messages', [])
