18e26f2c7e2ab59b6f997374ea274fda43ceea12
maja <mariusz.marek.olszewski@gmail.com>
2020-02-10 23:18:27 +0100

    both classes are working not returning proper proc name value

--- MakeScreenshot.py
+++ MakeScreenshot.py
@@ -1,7 +1,7 @@
 import win32gui
-import PIL
 import os
 import wmi
+from PIL import Image, ImageGrab 
 class ProcessFinder:
     def __init__(self):
         self._HWND           = win32gui.GetForegroundWindow()
@@ -42,12 +42,11 @@ class MakeScreenshot(ProcessFinder):
     def __init__(self, root_location = ".\\images"):
         super().__init__()
         
-        self._imageGrab     = PIL.ImageGrab # for screenshots
+        self._imageGrab     = ImageGrab # for screenshots
         self._file_list     = []
         self._root_location = root_location
     def make_screensots(self, parameters):
-        super().update_window_info()
-        
+        super().update_window_info()        
         time_stamp, order_num, sequence_num = parameters
         
         dirImage  = self._root_location + "\\{}\\".format(sequence_num)
--- mouse_keybord_events.py
+++ mouse_keybord_events.py
@@ -21,36 +21,43 @@ class TypeEvents:
     def on_release(self, key): 
         if key == self._stop_button:
             print('{0} released, stopping recording'.format(key))
-            return [False]
+            return False
         return [True,key, self._last_x, self._last_y, self._KEY_RELEASE]
     def on_press(self, key):        
         if key == self._stop_button:
             print('{0} pressed, stopping recording'.format(key))
-            return [False]
+            return False
         return [True,key, self._last_x, self._last_y, self._KEY_PRESS]
 
 class ControllerEvents(TypeEvents): 
     def __init__(self, stop_key=keyboard.Key['shift']):
         super().__init__(stop_key)
-        self.num_of_click = 0        
+        self.num_of_click = 0     
+    def get_time_stamp(self):
+            return time.strftime("%H%M%S")
+    def order_num(self):        
+        self.num_of_click += 1
+        return self.num_of_click   
+    def add_data(self, results):
+        results.append(self.get_time_stamp())
+        results.append(self.order_num())
     def on_click(self, x, y, button, pressed):
         if pressed:            
             results = super().on_click(x, y, button, pressed)            
-            results.append(self.get_time_stamp(), self.order_num())            
+            self.add_data(results)            
             return results
     def on_release(self, key):  # this is for the future        
         results = super().on_release(key)
-        if not results[0]: return [results[0]]            
-        else : return results.append(self.get_time_stamp(),self.order_num())
+        if not results: return results            
+        else : 
+            self.add_data(results)
+            return results
     def on_press(self, key):
         results = super().on_press(key)
-        if not results[0]: return [results[0]]
-        return results.append(self.get_time_stamp(), self.order_num())        
-    def get_time_stamp(self):
-        return time.strftime("%H%M%S")
-    def order_num(self):        
-        self.num_of_click += 1
-        return self.num_of_click
+        if not results: return results
+        else :
+            self.add_data(results)
+            return results
 
 class MouseKeyboardEvents(ControllerEvents): #this class is responsible for recording and running the "screenshot" class
     def __init__(self, screen_shot_class: MakeScreenshot , sequence_num=0, events_list={}, stop_key=keyboard.Key['shift']):
@@ -61,22 +68,24 @@ class MouseKeyboardEvents(ControllerEvents): #this class is responsible for reco
         self.start_recording()
     def on_click(self, x, y, button, pressed):
         results = super().on_click(x,y,button,pressed)        
-        if results[0]:            
+        if results:            
             self.add_do_dictionary(results)
+        else : return results
     def on_press(self,key):
         results = super().on_press(key)
-        if results[0]:            
+        if results:            
             self.add_do_dictionary(results)
+        else : return results
     def add_do_dictionary(self, results):
-            dict_key = str(results)
+            dict_key = str(results)            
             # 0           1     2    3      4               5          6         7
             #true/false, key, pos_x,pos_y,pressedName, timestamp, order_num, sequence_num
             if dict_key not in self._list_of_steps:
-                print(results)
-                self._list_of_steps[dict_key] = results.append(self._sequence_num)
-                if results[4] == self._MOUSE:
-                    parameters_for_screenshot = results[5:7:1]                    
-                    self.ScreenShots.make_screenshots(parameters_for_screenshot)
+                results.append(self._sequence_num)
+                self._list_of_steps[dict_key] = [results]
+                if results[4] == self._MOUSE:                
+                    parameters_for_screenshot = results[5:8:1]
+                    self.ScreenShots.make_screensots(parameters_for_screenshot)
     def start_recording(self):
         with MouseListener(on_click=self.on_click) as listener:
             with KeyboardListener(on_press=self.on_press) as listener:
@@ -85,4 +94,5 @@ from MakeScreenshot import MakeScreenshot
 steps    = {}
 sequence = 0
 stopKey = keyboard.Key['shift']
+print ("start test")
 test = MouseKeyboardEvents(MakeScreenshot(),sequence,steps,stopKey)
