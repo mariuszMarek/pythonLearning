15d560bed031de22997948d1afb4a403e846a478
maja <mariusz.marek.olszewski@gmail.com>
2020-02-23 13:18:46 +0100

    added new file with classes

---
+++ compare_screenshots.py
@@ -0,0 +1,13 @@
+from MakeScreenshot import MakeScreenshot
+
+class CompareImages:
+    def __init__(self):
+        pass    
+    def load_images(self, image_1, image_2):
+        pass
+    def compare_images(self):
+        pass
+    def calculate_mean_square_diff(self):
+        pass
+    def calculate_structural_similarity_index(self):
+        pass
