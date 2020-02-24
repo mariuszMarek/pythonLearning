650b9481a2d1f3ec721a256eccd5bf1fa0f63640
maja <mariusz.marek.olszewski@gmail.com>
2020-02-24 08:17:54 +0100

    fixed error with saving config info

--- ManagaSettings.py
+++ ManagaSettings.py
@@ -21,26 +21,24 @@ class ManageSettings(RootLocation):
         if stop_key != keyboard.Key['shift']:
             try:
                 if len(stop_key) > 1: stop_key = keyboard.Key[stop_key]
-            except:                
+            except:
                 pass
-        self.config_file   = self.ini_path + str(sequence) + self.ini_file_name
+        self.config_file   = self.ini_path + "\\" + str(sequence) + self.ini_file_name
         self.position_list = {}  # need to clear the list
-        #need to overwrite the settings so if we are missing the ini file it will start recording              
+        #need to overwrite the settings so if we are missing the ini file it will start recording
         MouseKeyboardEvents(MakeScreenshot(),sequence, self.position_list,stop_key)
         if self.position_list:
             self.save_settings(sequence)
-            # return self.position_list # do I need this?
-
     def settings_loader(self, load_all_or_one=False, sequence=0):
         #potrzebuje zerowanie listy
-        self.config_file   = self.ini_path + str(sequence) + self.ini_file_name               
-        if not Path(self.config_file).is_file() : self.record_settings(sequence)        
+        self.config_file   = self.ini_path + str(sequence) + self.ini_file_name
+        if not Path(self.config_file).is_file() : self.record_settings(sequence)
         return self.load_settings(load_all_or_one, sequence)
 
-    def save_settings(self, sequence = 0):        
+    def save_settings(self, sequence = 0):
         with open(self.config_file, 'w') as file_writter:
-            for multipleElements in self.position_list:                
-                joined_line      = ";".join(map(str,self.position_list[multipleElements][0]))                
+            for multipleElements in self.position_list:
+                joined_line      = ";".join(map(str,self.position_list[multipleElements][0]))
                 file_writter.write("{}\n".format(joined_line))
 
     def load_settings(self, return_all_or_one = False, return_sequence = 0):
@@ -50,10 +48,10 @@ class ManageSettings(RootLocation):
                 with open(self.ini_path + files, 'r') as file_reader:
                     for line_XY in file_reader:
                         elements_line = line_XY.strip().split(";")
-                        sequence_num  = elements_line[7]                        
+                        sequence_num  = elements_line[7]
                         if not sequence_num in sequence_dict:
                             sequence_dict[sequence_num] = [elements_line] 
                         else:
                             sequence_dict[sequence_num].append(elements_line)
-        if return_all_or_one: return sequence_dict        
+        if return_all_or_one: return sequence_dict
         return sequence_dict[str(return_sequence)] if str(return_sequence) in sequence_dict else ["missing given sequence for num",return_sequence]
